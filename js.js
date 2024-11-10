96efaaace15b4631c64f4d0d273360aafecb0f9e
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2024-11-11 00:59:10 +0800

    Add color opacity

--- js.js
+++ js.js
@@ -1,15 +1,25 @@
 let size = 16;
 let canvas = document.body.querySelector(".canvas")
+let resizeButton = document.body.querySelector("#resize")
 
 draw()
 
 document.body.addEventListener("mouseover", (e) => {
     let currentElement = e.target
-    if (currentElement.id) {
+    if (currentElement.id && currentElement.id != "resize") {
         currentElement.style.backgroundColor = `rgb(${randInt(0, 255)}, ${randInt(0, 255)}, ${randInt(0, 255)})`
+        if (currentElement.style.opacity < 1) currentElement.style.opacity = Number(currentElement.style.opacity) + Number(0.1);
     }
 })
 
+resizeButton.addEventListener("click", () => {
+    size = prompt("Enter new grid size: (1 ~ 100)")
+    while (size < 1 || size > 100) {
+        size = prompt("Invalid size! Enter new grid size: (1 ~ 100)")
+    }
+    canvas.innerHTML = ""
+    draw()
+})
 
 
 function randInt(min, max) {
@@ -24,6 +34,7 @@ function draw(){
             let cell = document.createElement("div");
             cell.setAttribute("class", "cell")
             cell.setAttribute("id", i*size+j);
+            cell.style.opacity = 0;
             row.appendChild(cell)
         }
         canvas.appendChild(row)
