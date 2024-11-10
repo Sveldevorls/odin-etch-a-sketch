e748dc2d5bb26b23d1a7139b749dce3ab1e237b0
Sveldevorls <64679979+Sveldevorls@users.noreply.github.com>
2024-11-10 23:14:57 +0800

    Add cell background color on hover

--- js.js
+++ js.js
@@ -3,6 +3,19 @@ let canvas = document.body.querySelector(".canvas")
 
 draw()
 
+document.body.addEventListener("mouseover", (e) => {
+    let currentElement = e.target
+    if (currentElement.id) {
+        currentElement.style.backgroundColor = `rgb(${randInt(0, 255)}, ${randInt(0, 255)}, ${randInt(0, 255)})`
+    }
+})
+
+
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
 function draw(){
     for (let i = 0; i < size; i++){
         let row = document.createElement("div");
